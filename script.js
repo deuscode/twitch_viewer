0f03b199ec7e1b81dfeb966ceb2a3f74b4419fa8
deuscode <deuscode87@gmail.com>
2017-05-14 23:04:09 -0400

    barebones JS

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $.ajax({
+        type: 'GET',
+        url: '',
+        headers: '',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+});
