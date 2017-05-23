6f46611f87d721a600c7056cbdc787c5e1cb50cd
deuscode <deuscode87@gmail.com>
2017-05-23 00:02:02 -0400

    Add script src in html and create boilerplate JS file

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
 
     $.ajax({
         type: 'GET',
-        url: '',
-        headers: '',
+        url: 'https://wind-bow.glitch.me/twitch-ap/streams/freecodecamp?callback=?',
+        header: {'Client-ID': 'hs5elqgum44iq9qdg3a1a3ydpyp00b'},
         success: function(data) {
             console.log(data);
+        },
+        error: function(err) {
+            console.log("error");
         }
     });
 });
