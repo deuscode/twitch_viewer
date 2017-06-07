5df28e636aa9eb648134481cac57e718603aa56d
deuscode <deuscode87@gmail.com>
2017-06-07 00:28:18 -0400

    change parameter to channels

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // global variables
-var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var apiURL = 'https://kaypeter.com/homebase/streams/'
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var apiURL = 'https://kaypeter.com/homebase/channels/'
 
-// iterate through streams
-for (var i = 0; i < streams.length; i++) {
+// iterate through channels
+for (var i = 0; i < channels.length; i++) {
     ajaxCall();
 };
 // make api call to backend server
 function ajaxCall() {
     $.ajax({
         type: 'GET',
-        url: apiURL + streams[i] + '?callback=?',
+        url: apiURL + channels[i] + '?callback=?',
         success: function (data) {
             console.log(data);
         },
