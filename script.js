bb934b19977305b3893131b718714a0942db2a1f
deuscode <deuscode87@gmail.com>
2017-06-06 20:52:40 -0400

    change API URL

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-$(document).ready(function() {
-    // make api call to backend server
+// global variables
+var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var apiURL = 'https://kaypeter.com/homebase/streams/'
+
+// iterate through streams
+for (var i = 0; i < streams.length; i++) {
+    ajaxCall();
+};
+// make api call to backend server
+function ajaxCall() {
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:11646/streams/freecodecamp',
-        success: function(data) {
+        url: apiURL + streams[i] + '?callback=?',
+        success: function (data) {
             console.log(data);
         },
-        error: function(err) {
+        error: function (err) {
             console.log('error');
         }
     });
-});
+};
