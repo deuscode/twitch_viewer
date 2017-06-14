cf9ccc5b9a899227d6a8b601d496e56c0b2b1932
deuscode <deuscode87@gmail.com>
2017-06-13 22:10:59 -0400

    add append method to test data on html

--- js/script.js
+++ js/script.js
@@ -4,22 +4,26 @@ var apiURL = 'https://kaypeter.com/homebase/channels/'
 
 // iterate through channels
 channels.forEach(channel => ajaxCall(channel));
-// make api call to backend server
+// make api call to my backend server
 function ajaxCall(channel) {
     $.ajax({
         type: 'GET',
         url: apiURL + channel,
         success: function (data) {
             console.log(data);
+            // append data to html
+            $("#twitchbox").append('<p>' + data.display_name + ' - ' + data.status + '</p>');
         },
+        // catch error and append to html
         error: function (err) {
             console.log('error', err);
+            $("#twitchbox").append('<p>' + data.status + '</p>');
         }
     });
 };
 
 $(document).ready(function() {
-
+ 
 });
 // test localStorage method for saving data
 // localStorage.setItem('channels', JSON.stringify(channels))
