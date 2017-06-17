0aabef400e0c09217d3e02a68d277123ba489e6e
deuscode <deuscode87@gmail.com>
2017-06-16 20:13:25 -0400

    add additional channels

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // global variables
-var channels = ["ESL_SC2", "OgamingSC2", "syntag", "freecodecamp", "hardlydifficult", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channels = ["ESL_SC2", "OgamingSC2", "syntag", "freecodecamp", "hardlydifficult", "habathcx", "RobotCaleb", "noobs2ninjas", "Lost_In_House", "serpent_AI"];
 var apiURL = 'https://kaypeter.com/homebase/channels/'
 
 // iterate through channels
@@ -12,7 +12,7 @@ function ajaxCall(channel) {
         success: function (data) {
             console.log(data);
             // append data to html
-            $("#twitchbox").append('<img src="' + data.logo + '">');
+            $("#twitchbox").append('<a href="#" target="_blank"><img src="' + data.logo + '"></a>');
         },
         // catch error and append to html
         error: function (err) {
