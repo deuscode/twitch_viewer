a3087ac7d8a38b3065873ab78f5c505582b33441
deuscode <deuscode87@gmail.com>
2017-06-28 22:40:59 -0400

    add on click function

--- js/script.js
+++ js/script.js
@@ -2,29 +2,38 @@
 var channels = ["ESL_SC2", "OgamingSC2", "syntag", "freecodecamp", "hardlydifficult", "habathcx", "RobotCaleb", "noobs2ninjas", "Lost_In_House", "serpent_AI"];
 var apiURL = 'https://kaypeter.com/homebase/channels/'
 
-// iterate through channels
-channels.forEach(function (channel) {
-    return ajaxCall(channel);
-});
-// make api call to my backend server
-function ajaxCall(channel) {
-    $.ajax({
-        type: 'GET',
-        url: apiURL + channel,
-        success: function (data) {
-            console.log(data);
-            // append data to html
-            $("#twitchbox").append('<a href="' + data.url + '" target="_blank"><img src="' + data.logo + '"></a>');
-        },
-        // catch error and append to html
-        error: function (err) {
-            console.log('error', err);
-            $("#twitchbox").append('<p>' + err + '</p>');
-        }
-    });
-};
-
 $(document).ready(function () {
+    // iterate through channels
+    channels.forEach(function (channel) {
+        return ajaxCall(channel);
+    });
+    // make api call to my backend server
+    function ajaxCall(channel) {
+        $.ajax({
+            type: 'GET',
+            url: apiURL + channel,
+            success: function (data) {
+                if (data.status >= 400) {
+                    alert(data.message);
+                } else {
+                    console.log(data);
+                    // append data to html
+                    $("#twitchbox").append('<a data-href="' + data.url + '" target="_blank"><img src="' + data.logo + '"></a>');
+                }
+            },
+        });
+    };
+
+    $("#addchannel-button").on('click', function (channel) {
+        var channel = $("#addchannel-box").val();
+        if (!channel) {
+
+        }
+        ajaxCall(channel);
+        $("#addchannel-box").val('');
+        var test = $('a')
+        console.log('array of channels', test[0])
+    })
 
 });
 // test localStorage method for saving data
